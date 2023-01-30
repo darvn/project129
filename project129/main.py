ea81f0758c3aa99310206afe26df68d9d9c7d6f5
darvn <86173198+darvn@users.noreply.github.com>
2023-01-30 14:40:53 -0700

    Add files via upload

---
+++ project129/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+#read csv file
+df = pd.read_csv("final.csv")
+#check number of rows and columns
+print(df.shape)
+
+# Delete columns
+
+#Rename column headers
+
+#Save the data into main.csv
